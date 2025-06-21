a8f41ad47b83c405b6af311a0797666a6636e02e
samruddhi Kulkarni <samruddhi672@gmail.com>
2025-06-21 11:18:19 +0530

    updated code with new repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
 	images: {
+		domains: ["localhost"], // also add IPs or domain names as needed
+
 		remotePatterns: [
 			{
 				hostname: "*",
@@ -10,13 +12,23 @@ const config = {
 	experimental: {
 		typedRoutes: false,
 	},
+
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/kisan-basket",
+				permanent: false,
+			},
+		];
+	},
 	// used in the Dockerfile
 	output:
 		process.env.NEXT_OUTPUT === "standalone"
 			? "standalone"
 			: process.env.NEXT_OUTPUT === "export"
-			  ? "export"
-			  : undefined,
+				? "export"
+				: undefined,
 };
 
 export default config;
